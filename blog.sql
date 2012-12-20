4ca036ef7786024731afb0586f9cf3ad89549cbf
Eorling <dzirt.do.urdenn@gmail.com>
2012-12-20 12:04:11 +0200

    sql update

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Дек 16 2012 г., 16:10
+-- Время создания: Дек 20 2012 г., 12:03
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -30,19 +30,22 @@ CREATE TABLE IF NOT EXISTS `article` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `article` text NOT NULL,
-  `date` date NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `author` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Дамп данных таблицы `article`
 --
 
 INSERT INTO `article` (`id`, `title`, `article`, `date`, `author`) VALUES
-(1, 'Новая статья', 'ПРосто тест', '2010-10-10', 'Borya'),
-(2, 'ещё одна статья', 'Ещё одна мега статья', '0000-00-00', 'Ашот'),
-(3, 'Как устроены переменные в PHP tutorial', 'Вроде простой вопрос, даже не понятно что на него ответить, правда?\r\nМы все знаем как создать переменную, как получить значение переменной, как взять ссылку на переменную в конце концов.\r\nНо как они работают изнутри?\r\nЧто происходит в интерпретаторе, когда вы изменяете значение переменной? Или когда удаляете ее?\r\nКак реализованы типы переменных?\r\n\r\nВ этой статье я постараюсь раскрыть именно эти темы.\r\n\r\nAbstract\r\n\r\nПеременные в PHP выражены в виде неких контейнеров, которые хранят в себе тип переменной, значение, кол-во ссылающихся переменных на этот контейнер, и флаг — является ли эта переменная ссылочной.', '2001-01-01', 'Гена');
+(1, 'Новая статья', 'ПРосто тест', '2010-10-09 21:00:00', 'Borya'),
+(2, 'ещё одна статья', 'Ещё одна мега статья', '0000-00-00 00:00:00', 'Ашот'),
+(3, 'Как устроены переменные в PHP tutorial', 'Вроде простой вопрос, даже не понятно что на него ответить, правда?\r\nМы все знаем как создать переменную, как получить значение переменной, как взять ссылку на переменную в конце концов.\r\nНо как они работают изнутри?\r\nЧто происходит в интерпретаторе, когда вы изменяете значение переменной? Или когда удаляете ее?\r\nКак реализованы типы переменных?\r\n\r\nВ этой статье я постараюсь раскрыть именно эти темы.\r\n\r\nAbstract\r\n\r\nПеременные в PHP выражены в виде неких контейнеров, которые хранят в себе тип переменной, значение, кол-во ссылающихся переменных на этот контейнер, и флаг — является ли эта переменная ссылочной.', '2000-12-31 22:00:00', 'Гена'),
+(4, 'dasd', 'asdasd', '0000-00-00 00:00:00', 'asd'),
+(5, 'dasd', 'asdasdasd', '0000-00-00 00:00:00', 'dzirt.do.uweeeerdenn@gmail.com'),
+(6, 'asdasd', 'asdasdasdasd', '2012-12-20 09:47:28', 'dzirt.do.uweeeerdenn@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -59,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`pk_user`),
   UNIQUE KEY `login` (`login`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 --
 -- Дамп данных таблицы `user`
@@ -71,7 +74,10 @@ INSERT INTO `user` (`pk_user`, `dtime_registration`, `login`, `password`, `email
 (15, 1355593124, 'qwertyu', '25f9e794323b453885f5181f1b624d0b', 'qwerty@mail.ru'),
 (16, 1355593453, 'qwerty', '25f9e794323b453885f5181f1b624d0b', 'qwertyr@mail.ru'),
 (17, 1355593514, 'qwertyuu', '25f9e794323b453885f5181f1b624d0b', 'qwertyruu@mail.ru'),
-(18, 1355593574, 'qwertyuud', '25f9e794323b453885f5181f1b624d0b', 'qwertyruud@mail.ru');
+(18, 1355593574, 'qwertyuud', '25f9e794323b453885f5181f1b624d0b', 'qwertyruud@mail.ru'),
+(19, 1355817854, 'Adept', '9d53c861c64cf020341c473f93ab3b7d', 'dzirt.do.urdenn@gmail.com'),
+(20, 1355843859, 'Adeptqqq', '343b1c4a3ea721b2d640fc8700db0f36', 'dzirt.doqq.urdenn@gmail.com'),
+(21, 1355845824, 'Adeptaew', 'efe6398127928f1b2e9ef3207fb82663', 'dzirt.do.uweeeerdenn@gmail.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
