8995928d06203486966bde3d1b1059eed07e067d
Eorling <dzirt.do.urdenn@gmail.com>
2013-01-14 12:18:59 +0200

    Mysql Dump

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Янв 13 2013 г., 11:41
+-- Время создания: Янв 14 2013 г., 12:17
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -19,6 +19,41 @@ SET time_zone = "+00:00";
 --
 -- База данных: `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `article`
+--
+
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `article` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `author` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+--
+-- Дамп данных таблицы `article`
+--
+
+INSERT INTO `article` (`id`, `title`, `article`, `date`, `author`) VALUES
+(3, 'Как устроены переменные в PHP tutorial', 'Вроде простой вопрос, даже не понятно что на него ответить, правда?\nМы все знаем как создать переменную, как получить значение переменной, как взять ссылку на переменную в конце концов.\nНо как они работают изнутри?\nЧто происходит в интерпретаторе, когда вы изменяете значение переменной? Или когда удаляете ее?\nКак реализованы типы переменных?\n\nВ этой статье я постараюсь раскрыть именно эти темы.\n\nAbstract\n\nПеременные в PHP выражены в виде неких контейнеров, которые хранят в себе тип переменной, значение, кол-во ссылающихся переменных на этот контейнер, и флаг — является ли эта переменная ссылочной.', '2000-12-31 22:00:00', 'Гена'),
+(4, 'dasd', 'asdasd', '0000-00-00 00:00:00', 'asd'),
+(5, 'dasd', 'asdasdasd', '0000-00-00 00:00:00', 'dzirt.do.uweeeerdenn@gmail.com'),
+(6, 'asdasd', 'asdasdasdasd', '2012-12-20 09:47:28', 'dzirt.do.uweeeerdenn@gmail.com'),
+(7, 'asd', 'asd', '2012-12-22 20:45:35', 'dzirt.do.urdsenn@gmail.com'),
+(8, 'cvb', 'cvb', '2013-01-02 14:30:42', 'dzirt.do.urdsenn@gmail.com'),
+(9, 'asd', '987987879', '2013-01-09 12:41:15', 'admin@admin.ua'),
+(10, 'sdasdasdasdasd', 'asdasdasdasdasdasdasdasdasd', '2013-01-09 19:58:38', 'admin@admin.ua'),
+(11, 'asd', 'asd', '2013-01-09 20:07:20', 'admin@admin1.ua'),
+(12, 'dasd', '122', '2013-01-09 20:41:29', 'dzirt.do.urdenn@gmail.coms'),
+(13, 'asd', 'ertetetertertert', '2013-01-10 07:10:41', 'dzirt.do.urdenn@gmail.coms'),
+(14, 'dasd', 'fghfgh', '2013-01-11 14:20:21', 'Eamail@va.ru'),
+(15, 'sdasdasdasdasd', 'sdfsdfsdf', '2013-01-11 15:44:49', 'admin@admin1.uaasd'),
+(16, 'ny', 'article', '2013-01-13 09:29:42', 'admin@admin1.uaasd');
 
 -- --------------------------------------------------------
 
